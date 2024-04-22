a05ab047369649c587974323d01bc950bc1ef588
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2024-04-22 23:01:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List, Dict
 
 origins = ["*"]
